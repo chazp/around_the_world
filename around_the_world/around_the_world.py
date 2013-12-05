c7071a32279a96c21740db92a3fcf51245e8cf54
Chaz Chamberlin <chaz@Pip.(none)>
2013-12-04 17:49:40 -0800

    Worked on play() function. It can now play a game of Around the World. Added variable and code if player chances every shot

--- around_the_world/around_the_world.py
+++ around_the_world/around_the_world.py
@@ -9,16 +9,17 @@ from random import randint
 class AroundtheWorld():
     def __init__(self):
         self.current_location = 'start'
+        self.chance_it = False
         self.shots = ["start", "right_corner", "right_side",
                  "middle", "left_side", "left_corner", "end"]
         self.shot_percentage = {
-            "start": 50,
-            "right_corner": 50,
-            "right_side": 50,
-            "middle": 50,
-            "left_corner": 50,
-            "left_side": 50,
-            "end": 50
+            "start": 90,
+            "right_corner": 70,
+            "right_side": 70,
+            "middle": 70,
+            "left_corner": 70,
+            "left_side": 70,
+            "end": 90
         }
 
     def get_next_location(self, current_location):
@@ -26,8 +27,8 @@ class AroundtheWorld():
         Returns next shot. If current_shot is "end" 
         returns "win"
         """
-            return (self.shots[current_location + 1] 
-                    if current_location is not "end" else "win")
+        current_index = self.shots.index(current_location)
+        return(self.shots[current_index + 1] if (current_location != "end") else 'win')
 
     def made_shot(self, current_location):
         """
@@ -36,19 +37,24 @@ class AroundtheWorld():
         """
         return (randint(1, 100) <= self.shot_percentage[current_location])
 
-    def play():
-        current_location = "end"
+    def play(self):
+        current_location = "start"
+        turns = 0
         shot = False
-        while (current_location != "end"):
+        print("Are you chancing it?: %s" % self.chance_it)
+        while (current_location != "win"):
             shot = self.made_shot(current_location)
-            print("Shot at %s: %s" % (current_location, shot))
-            if (shot)
+            if (self.chance_it):
+                shot = shot or self.made_shot(current_location) # shoot again
+            print("You shoot at %s. Results: %s" % (current_location, shot))
+            turns += 1
+            if (shot):
                 current_location = self.get_next_location(current_location)
-        print("You win!")
+            elif (self.chance_it): # player chanced it and missed both
+                current_location = "start"
+        print("You win in only %d turns!" % turns)
             
                     
 
 test = AroundtheWorld()
-print(test.shot_percentage["right_side"])
-for i in range(10):
-    print(test.made_shot("start"))
+test.play()
